ba5fa87c7c1f9f594c8a8a4be6dc0fd223c7025f
Bhavya Shah <woburnp@gmail.com>
2023-09-16 01:14:03 -0400

    Issue with Image Centering Resolved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className = "flex justify-center items-center h-screen w-screen">
-           <Image  src = "/self-portrait.jpg" alt="Self Portrait" width={100} height = {50}>
+           <Image  src = "/image.jpg" alt="Self Portrait" width={100} height = {50}>
 
           </Image>
     </div>
