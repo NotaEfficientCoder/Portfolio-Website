43b076aa94aff0d3b497bc9fa92311b3b8767716
Bhavya Shah <woburnp@gmail.com>
2023-09-18 17:50:42 -0400

    Added navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import Image from 'next/image'
+import Github from 'public/github.png'
+import Linkedin from 'public/linkedin.png'
+
+
 
 export default function Home() {
   return (
     <div>
-       Hi, my name is Bhavya Shah. Welcome to my website. This is a work in progress.
+        <header className = "header">
+           <div className = "name">Bhavya Shah</div>
+           <div className = "right-side">
+             <a href = "/Bhavya_Shah_Resume.pdf" target = "_blank"className="resume-button">Resume</a>
+             <a href="https://www.linkedin.com/in/shahbhavyakumar/" target="_blank">
+              <Image className = "github-icon" src = {Linkedin} alt = "Linkedin" ></Image>
+             </a>
+             <a href="https://github.com/bh4vya" target="_blank">
+              <Image className = "github-icon" src = {Github} alt = "Github" ></Image>
+             </a>
+           </div>
+        </header>
+       
     </div>
+
+    
   )
 }
+
