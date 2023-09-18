4c4a04bf1393bd33123b3b1ab0a2aedc844af7dc
Bhavya Shah <woburnp@gmail.com>
2023-09-17 23:23:07 -0400

    Blank Slate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className = "flex justify-center items-center h-screen w-screen">
-           <Image  src = "/image.jpg" alt="Self Portrait" width={100} height = {50}>
-
-          </Image>
+    <div>
+       
     </div>
- 
   )
 }
