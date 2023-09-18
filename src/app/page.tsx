252d6b2b9298066201da00272c35e2b88918940c
Bhavya Shah <woburnp@gmail.com>
2023-09-17 23:50:07 -0400

    Updated launch page message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-       
+       Hi, my name is Bhavya Shah. Welcome to my website. This is a work in progress.
     </div>
   )
 }
