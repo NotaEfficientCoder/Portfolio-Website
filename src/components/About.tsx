7eb468172e8b50600c21bd0190d107df7d7002d3
Bhavya Shah <woburnp@gmail.com>
2023-09-18 20:19:59 -0400

    Fixed margins and padding of icons

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,9 +11,9 @@ const About = () => {
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat voluptatum adipisci maxime? Exercitationem cum unde blanditiis ipsam illum tenetur labore eum aut natus. Dignissimos, dolore magnam voluptatum nostrum magni vel reiciendis perspiciatis sint odio quibusdam accusantium ducimus, doloremque dolorum incidunt eveniet praesentium. Doloremque sed perferendis recusandae ad explicabo dicta, veniam commodi officia id sint facere repellendus. Ratione maiores omnis impedit facere fugiat, vitae modi excepturi, architecto quisquam cum laboriosam ipsum officiis, maxime doloremque. Sit facilis accusamus placeat voluptatibus rerum culpa molestiae odio dolorem architecto neque officiis expedita recusandae, magni, blanditiis repudiandae aliquid totam. Vitae quisquam veritatis maiores optio fuga iusto velit asperiores amet cupiditate.</p>
         </div>
         <div className = "border-2 border-black w-1/2 flex flex-row">
-            <BiLogoTailwindCss className = "w-20 h-20 bg-blue-500 rounded-full"/>
-            <AiFillHtml5 className = "w-20 h-20"/>
-            <SiNextdotjs className = "w-[75px] h-[75px] pt-2 ml-1"/>
+            <BiLogoTailwindCss className = "w-20 h-20 bg-blue-500 p-2 m-1 rounded-full"/>
+            <AiFillHtml5 className = "w-20 h-20 bg-orange-500 p-2 m-1 rounded-full"/>
+            <SiNextdotjs className = "w-[75px] h-[75px] p-2 m-1 bg-black rounded-xl"/>
         </div>
         <div className='w-48 h-48 justify-center overflow-hidden'>
             <Image className = "rounded-full relative object-cover" width = {170} height= {170} src = "/bhavya.png" alt = "bhavya" ></Image>
